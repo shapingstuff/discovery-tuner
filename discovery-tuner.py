0a303bcaa4564f716b01c6c179ca82ca7bd42a7e
shapingstuff <tommydykes@gmail.com>
2020-02-17 20:14:54 +0000

    vlc added

--- discovery-tuner.py
+++ discovery-tuner.py
@@ -5,6 +5,7 @@ import binhex
 import math
 import time
 import random
+import vlc
 
 zone_size=50
 canvas_dimensions=(800,600)
@@ -22,6 +23,17 @@ radiourl = [
     'http://ice2.somafm.com/lush-32-aac',
     'http://bbcwssc.ic.llnwd.net/stream/bbcwssc_mp1_ws-eieuk',
     ]
+
+#vlc
+instance=vlc.Instance('--input-repeat=-1')
+players = []
+for x in radiourl:
+    players.append(instance.media_player_new())
+
+#set all players to off
+play =[]
+for x in radiourl:
+    play.append(0)
 
 def read_mouse():
     f = open( "/dev/input/mice", "rb" )
@@ -59,10 +71,22 @@ while True:
 
     index=0
     for x in zones:
-        index=index+1
         c = math.sqrt((x.x-position_x)*(x.x-position_x) + (x.x-position_y)*(x.x-position_y))
         if c <= zone_size:
             p = c/zone_size*100
             p = int(100-p)
             print("Zone "+str(index)+" active at "+str(p)+" %")
-            
+            players[index].audio_set_volume(p)
+            if play[index]==0:
+                print("playing" + str(index))
+                play[index]=1
+                print(radiourl[index])
+                media=instance.media_new(radiourl[index])
+                #Set player media
+                players[index].set_media(media)
+                #Play the media
+                players[index].play()
+        else:
+            play[index]=0
+            players[index].stop()
+        index = index+1
